841efebb60bdd62f365cd7d898e25af1c9eaae97
rakeshkumar <sravanamrakeshkumar3@gmail.com>
2025-03-04 15:27:13 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist' // Ensures Vercel uses the correct output folder
   },
+  server: {
+    port: 3000
+  }
 });
+
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
